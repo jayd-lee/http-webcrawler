8632dc7d3ac57162cb487722f98c02b436fbc94a
Jayden Lee <jaydeh.lee@gmail.com>
2024-02-25 22:49:26 -0800

    crawlpage recursively

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { crawlPage } = require("./crawl")
 
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("no valid website provided")
     process.exit(1)
@@ -12,7 +12,11 @@ function main() {
   }
   const baseURL = process.argv[2]
   console.log(`starting crawl ${baseURL}`)
-  crawlPage(baseURL)
+  const pages = await crawlPage(baseURL, baseURL, {})
+
+  for (const page of Object.entries(pages)) {
+    console.log(page)
+  }
 }
 
 main()
